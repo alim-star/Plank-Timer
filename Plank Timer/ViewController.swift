5f7490aa66570af82e1b208b794c5869ba729557
Muhammad Ali <alim13@mcmaster.ca>
2022-03-03 08:03:59 -0500

    final

--- Plank Timer/ViewController.swift
+++ Plank Timer/ViewController.swift
@@ -6,13 +6,58 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var audioPLayer: AVAudioPlayer!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    let plankTimes = [ "30" : 30, "60" : 60, "90": 90 ]
+    
+    var timer = Timer()
+    var totalTime = 0
+    var secondspassed = 0
+    
+    @IBAction func plankSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        //when new button is pressed, previous timer is stopped
+        
+    let plank = sender.currentTitle!
+        
+    totalTime = plankTimes[plank]!
+        
+        
+        progressBar.progress = 0.0
+        // progress bar comes back to 0 whenever new button is pressed
+        secondspassed = 0
+        // seconds return back to 0
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
+    
+    func playSound() { let url = Bundle.main.url(forResource: "Alarm", withExtension: "wav")
+        audioPLayer = try!AVAudioPlayer(contentsOf: url!)
+        audioPLayer.prepareToPlay()
+        audioPLayer.play()
+        // plays designated sound named Alarm with the extension of wav
+    }
+    
+    @objc func updateTimer() {
+        if secondspassed < totalTime {
+                secondspassed += 1
+            
+                progressBar.progress = Float(secondspassed) / Float (totalTime)
+            
+        } else {
+            timer.invalidate()
+            playSound()
+            
+        }
+    }
+
 
 
 }
